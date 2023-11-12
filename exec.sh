cecd971471ef51f281d48884871694c5f6b6bcfc
spantheslayer <supriyosarkar282@gmail.com>
2023-11-12 18:01:02 +0530

    autoscript 🚀

---
+++ exec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the alias
+alias_line='alias p="$HOME/gitpush.sh"'
+
+# Check for .zshrc and append the alias
+if [ -f "$HOME/.zshrc" ]; then
+    echo $alias_line >> $HOME/.zshrc
+    echo "Alias added to .zshrc"
+# Check for .bashrc and append the alias
+elif [ -f "$HOME/.bashrc" ]; then
+    echo $alias_line >> $HOME/.bashrc
+    echo "Alias added to .bashrc"
+# If neither file exists, create .bashrc and append the alias
+else
+    touch $HOME/.bashrc
+    echo $alias_line >> $HOME/.bashrc
+    echo ".bashrc file created and alias added"
+fi
